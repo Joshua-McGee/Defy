356f509eb1d8fa873a5c123d48bfb1414f299caf
Joshua-McGee <jcooler.cooler@gmail.com>
2019-08-27 21:14:54 +0000

    updated the create page to have the input fields and buttons all styled with css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
+
 $(() => {
   $.ajax({
     method: "GET",
@@ -7,4 +9,19 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+});
+
+// updates the html of the textarea placeholder
+document.getElementById("first-submit").onclick = updateTextarea1;
+console.log("This is the button being clicked", document.getElementById("first-submit").onclick);
+
+function updateTextarea1 () {
+  const numOfPpl = document.getElementById('challenge-number').innerText;
+  document.getElementById("challenge-number").placeholder = numOfPpl;
+};
+
+// $('input[name=radio]:radio').on('change', function() {
+//   $('#target').val( $(this).val() );
+// });
+
 });
