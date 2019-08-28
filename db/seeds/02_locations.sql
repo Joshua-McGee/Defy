b5bbcd2a5bdc4b2c3c5aa6a2d5f69e0d01d0ac1e
Gary <garyyue2017@gmail.com>
2019-08-28 00:42:33 +0000

    finished challenges page

--- db/seeds/02_locations.sql
+++ db/seeds/02_locations.sql
@@ -1,3 +1,3 @@
-INSERT INTO locations (name, lat, long) VALUES ('downtown', 49.2820, -123.1171);
-INSERT INTO locations (name, lat, long) VALUES ('coquitlam', 49.2838, -122.7932);
-INSERT INTO locations (name, lat, long) VALUES ('kitsilano', 49.2684, -123.1683);
+INSERT INTO locations (name, lat, long) VALUES ('downtown', 49.2820, -123.1171, '999 Ave');
+INSERT INTO locations (name, lat, long) VALUES ('coquitlam', 49.2838, -122.7932, '999 Ave');
+INSERT INTO locations (name, lat, long) VALUES ('kitsilano', 49.2684, -123.1683, '999 Ave');
