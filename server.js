8bc74b46e581ce3cd92ddea8a3494b211b1e4c64
Gary <garyyue2017@gmail.com>
2019-08-28 20:42:29 +0000

    linking database

--- server.js
+++ server.js
@@ -48,12 +48,11 @@ app.use("/api/ranks", ranksRoutes(db));
 app.use("/api/landing", homepageRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
-
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("landing");
 });
 
 app.listen(PORT, () => {
