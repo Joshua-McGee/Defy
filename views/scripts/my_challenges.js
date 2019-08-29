43f3af3683a16bf47c4392f6c4e1907a77133864
Gary <garyyue2017@gmail.com>
2019-08-29 18:42:04 +0000

    changed node queries to jquery

---
+++ views/scripts/my_challenges.js
@@ -0,0 +1,273 @@
+  let map;
+  let bounds;
+  let arr;
+
+  function initMap(genre) {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13,
+        styles: [
+          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#263c3f'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#6b9a76'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#38414e'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#212a37'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9ca5b3'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#746855'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#f3d19c'}]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#2f3948'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#17263c'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#515c6d'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#17263c'}]
+          }
+        ]
+      });
+
+      bounds = new google.maps.LatLngBounds();
+
+      // let card = $('#pac-card');
+
+      // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+    $.get(`/api/challenges/genres/${genre ? genre : 'Everything'}`, (res) => {
+
+      arr = res.data.rows;
+
+      for (var key in arr) {
+
+        let data = arr[key];
+
+        let content = `
+          <div>Event: ${data.name}</div>
+          <div>Address: ${data.address}</div>
+        `;
+
+        let infowindow = new google.maps.InfoWindow({
+          content
+        });
+
+        let marker = new google.maps.Marker({
+          position: new google.maps.LatLng (data.lat, data.long),
+          map: map,
+          title: key
+        });
+
+        marker.addListener('hover', function() {
+          infowindow.open(map.marker);
+        });
+
+        marker.addListener('click', function() {
+          let modal = $('#modal');
+          let modalContent = $('#modal-content');
+          let newDiv = $('<div>');
+          let exit = $('<button>');
+          exit.addClass('exit');
+          exit.html('&times;');
+          modalContent.append(newDiv);
+
+          let headerDiv = $('<div>');
+          headerDiv.append($('<label>').text(`${data.name.charAt(0).toUpperCase() + data.name.substring(1, data.name.length)} - ${data.genre}`));
+          headerDiv.append(exit);
+          headerDiv.addClass('header');
+
+          let infoDiv = $('<div>');
+          infoDiv.addClass('info');
+
+          let dLDiv = $('<div>');
+          dLDiv.addClass('dl');
+          let dateDiv = $('<div>');
+          dateDiv.append($('<label>').text('Date'));
+          dateDiv.append($('<p>').text(`${data.date}`));
+          dateDiv.addClass('date');
+          let locationDiv = $('<div>');
+          locationDiv.append($('<label>').text('Location'));
+          locationDiv.append($('<p>').text(`${data.address}`));
+          locationDiv.addClass('location');
+
+          //appends date and location into date and location div
+          dLDiv.append(dateDiv);
+          dLDiv.append(locationDiv);
+
+          let descriptionDiv = $('<div>');
+          descriptionDiv.append($('<label>').text('Description'));
+          descriptionDiv.addClass('description');
+          descriptionDiv.append($('<p>').text(data.description));
+
+          //adds date location and description into a div
+          infoDiv.append(dLDiv);
+          infoDiv.append(descriptionDiv);
+
+          let acceptBtn = $('<button>');
+          acceptBtn.addClass('accept');
+          acceptBtn.text('Accept');
+
+          newDiv.append(headerDiv);
+          newDiv.append(infoDiv);
+          newDiv.append(acceptBtn);
+
+          exit.click(() => {
+            modal.removeClass('show');
+            modal.addClass('hide');
+            newDiv.remove();
+          });
+
+          modal.removeClass('hide');
+          modal.addClass('show');
+        });
+        bounds.extend(marker.position);
+      }
+      map.fitBounds(bounds); //-> use this one to show more than one point
+    });
+
+      function setupClickListener(id, types) {
+        var radioButton = $(`#${id}`);
+        radioButton.click(() => {
+          initMap(types[0]);
+        });
+      }
+
+      setupClickListener('everything', ['Everything']);
+      setupClickListener('physical', ['Physical']);
+      setupClickListener('mental', ['Mental']);
+      setupClickListener('game', ['Game']);
+      setupClickListener('random', ['Random']);
+
+  }
+
+  $('#pac-input').click(() => {
+    let modal = $('#modal');
+    let modalContent = $('#modal-content');
+    let newDiv = $('<div>');
+    let exit = $('<button>');
+    exit.addClass('exit');
+    exit.html('&times;');
+    modalContent.append(newDiv);
+
+    let headerDiv = $('<div>');
+    headerDiv.append($('<label>').text(`Select Type: `));
+    headerDiv.append(exit);
+    headerDiv.addClass('header');
+
+    newDiv.append(headerDiv);
+
+    let selectionDiv = $('<div>');
+    selectionDiv.addClass('selection');
+
+    let everythingInput = $(`
+    <div>
+      <input type="radio" name="filter" value="Everything">
+      <label>Everything</label>
+    </div>`);
+
+    let physicalInput = $(`
+    <div>
+      <input type="radio" name="filter" value="Physical">
+      <label>Physical</label>
+    </div>`);
+
+    let mentalInput = $(`
+    <div>
+      <input type="radio" name="filter" value="Mental">
+      <label>Mental</label>
+    </div>`);
+
+    let gameInput = $(`
+    <div>
+      <input type="radio" name="filter" value="Game">
+      <label>Game</label>
+    </div>`);
+
+    let randomInput = $(`
+    <div>
+      <input type="radio" name="filter" value="Random">
+      <label>Random</label>
+    </div>`);
+
+    selectionDiv.append(everythingInput);
+    selectionDiv.append(physicalInput);
+    selectionDiv.append(mentalInput);
+    selectionDiv.append(gameInput);
+    selectionDiv.append(randomInput);
+
+    newDiv.append(selectionDiv);
+
+
+    $('input[name=filter]:radio').on('change', (e) => {
+      $('#pac-input').val(e.target.value);
+      initMap(e.target.value);
+    });
+
+    exit.click(() => {
+      modal.removeClass('show');
+      modal.addClass('hide');
+      newDiv.remove();
+    });
+
+    modal.removeClass('hide');
+    modal.addClass('show');
+  });
